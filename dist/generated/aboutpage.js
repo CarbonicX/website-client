1a2cc80be73ad7635399afcce818edf19bcf8dfa
CarbonicX <carbonicx@qq.com>
2026-02-08 12:42:51 +0000

    Deploying to gh-pages from @ CarbonicX/website-client@4ae9d5f1ccc5148eab8ac42ad1fde80e0d22f718 🚀

--- dist/generated/aboutpage.js
+++ dist/generated/aboutpage.js
@@ -4,7 +4,7 @@ export class AboutPage extends silicon.ComplexWidgetBase {
     constructor(id, classes, date) {
         super(id, classes);
         this._element = document.createElement("div");
-        this.addChild(new silicon.Page("page", [], new silicon.VBox(null, [], [new silicon.Text(null, ["title"], "Welcome!"), new silicon.Text(null, [], "Here is the homepage of CarbonicX."), new silicon.Text(null, [], `So far, the website has been running well for ${date} days.`), new silicon.Button("button", [], "Get a message from server"), new silicon.HBox(null, [], [new silicon.Text(null, [], "Build with Silicon v1.0.0-alpha"), new silicon.Image(null, [], "assets/silicon.png")])]), "card"));
+        this.addChild(new silicon.Page("page", [], new silicon.VBox(null, [], [new silicon.Text(null, ["title"], "Welcome!"), new silicon.Text(null, [], "Here is the homepage of CarbonicX."), new silicon.Text(null, [], `So far, the website has been running well for ${date} days.`), new silicon.Button("button", [], "Get a message from server"), new silicon.HBox(null, [], [new silicon.Text(null, [], "Build with Silicon v1.0.0-alpha"), new silicon.Image(null, [], "assets/silicon.svg", 32, 32)])]), "card"));
         this.setIdAndClasses();
     }
 }
